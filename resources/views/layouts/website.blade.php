4ea85037a5bf06aecfdd25afaad1e43d8a353df8
SumitSinghssk <karakotisumitssk@gmail.com>
2026-09-21 20:45:15 +0530

    fonts downloaded and used

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -2,15 +2,58 @@
     @include('layouts.partials.website.seo')
 
     @push('head-scripts')
-        @vite(['resources/css/website.css', 'resources/js/app.js'])
+        <link rel="preload" href="{{ asset('fonts/geist-latin-600-normal.woff2') }}" as="font" type="font/woff2" crossorigin />
+        <link rel="preload" href="{{ asset('fonts/hanken-grotesk-latin-400-normal.woff2') }}" as="font" type="font/woff2" crossorigin />
 
         <style>
+            @font-face {
+                font-family: 'Geist';
+                font-style: normal;
+                font-weight: 400;
+                font-display: swap;
+                src: url('{{ asset('fonts/geist-latin-400-normal.woff2') }}') format('woff2');
+            }
+
+            @font-face {
+                font-family: 'Geist';
+                font-style: normal;
+                font-weight: 500;
+                font-display: swap;
+                src: url('{{ asset('fonts/geist-latin-500-normal.woff2') }}') format('woff2');
+            }
+
+            @font-face {
+                font-family: 'Geist';
+                font-style: normal;
+                font-weight: 600;
+                font-display: swap;
+                src: url('{{ asset('fonts/geist-latin-600-normal.woff2') }}') format('woff2');
+            }
+
+            @font-face {
+                font-family: 'Hanken Grotesk';
+                font-style: normal;
+                font-weight: 400;
+                font-display: swap;
+                src: url('{{ asset('fonts/hanken-grotesk-latin-400-normal.woff2') }}') format('woff2');
+            }
+
+            @font-face {
+                font-family: 'Hanken Grotesk';
+                font-style: normal;
+                font-weight: 500;
+                font-display: swap;
+                src: url('{{ asset('fonts/hanken-grotesk-latin-500-normal.woff2') }}') format('woff2');
+            }
+
             body {
                 min-height: 100vh;
                 display: flex;
                 flex-direction: column;
             }
         </style>
+
+        @vite(['resources/css/website.css', 'resources/js/app.js'])
     @endpush
 
     <a
